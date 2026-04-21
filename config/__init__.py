f8339148268266e8b1be8b4b31e1105f11ce9267
thermalox-ofc <cpbeoll@bicol-u.edu.ph>
2026-04-20 22:07:39 -0400

    Delete config directory

--- config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-配置模块初始化
-"""
-from .base import get_config, BaseConfig, DevelopmentConfig, ProductionConfig, TestingConfig
-
-__all__ = ['get_config', 'BaseConfig', 'DevelopmentConfig', 'ProductionConfig', 'TestingConfig'] 
